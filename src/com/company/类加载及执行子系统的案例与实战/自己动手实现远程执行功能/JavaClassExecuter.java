fcfd2723e82eb0cf8edbd383f1907e31a5e7bc2c
23832 <2383299053@qq.com>
2017-11-02 19:17:06 +0800

    自己动手实现远程执行功能

---
+++ src/com/company/类加载及执行子系统的案例与实战/自己动手实现远程执行功能/JavaClassExecuter.java
@@ -0,0 +1,28 @@
+package com.company.类加载及执行子系统的案例与实战.自己动手实现远程执行功能;
+
+import java.lang.reflect.Method;
+
+public class JavaClassExecuter {
+    /**
+     * 执行外部传过来的代表一个Java类的byte数组
+     * 将输入类的byte数组中代表java.lang.System的CONSTANT_Utf8_info常量修改为劫持后的HackSystem类
+     * 执行方法为该类的static main(String[] args)方法，输出结果为该类向System.out/err输出的信息
+     *
+     * @param classByte 代表一个java类的byte数组
+     * @return 执行结果
+     */
+    public static String execute(byte[] classByte) {
+        HackSystem.clearBuffer();
+        ClassModifier cm = new ClassModifier(classByte);
+        byte[] modiBytes = cm.modifyUTF8Constant("java/lang/System", "org/fenixsoft/classloading/execute/HackSystem");
+        HotSwapClassLoader loader = new HotSwapClassLoader();
+        Class clazz = loader.loadByte(modiBytes);
+        try {
+            Method method = clazz.getMethod("main", new Class[]{String[].class});
+            method.invoke(null, new String[]{null});
+        } catch (Throwable e) {
+            e.printStackTrace(HackSystem.out);
+        }
+        return HackSystem.getBufferString();
+    }
+}
